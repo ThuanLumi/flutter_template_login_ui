483a8c327c4bd05f584adf956a7b235b7f9b1378
Thuan Dinh Nam <thuandinhnam307@gmail.com>
2022-03-18 16:50:25 +0700

    Add flutter_template_login_ui folder

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_example_login_ui/screens/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Login UI',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
